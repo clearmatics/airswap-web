079c89c23d5c9d8926a8bb4c4060e351355cb803
Máté Szendrő <mate.szendro@clearmatics.com>
2024-06-18 15:35:51 +0100

    Only support Autonity networks

--- src/constants/supportedNetworks.ts
+++ src/constants/supportedNetworks.ts
@@ -1,13 +1,3 @@
 import { ChainIds } from "@airswap/utils";
 
-export const supportedNetworks: number[] = [
-  ChainIds.MAINNET,
-  ChainIds.LINEA,
-  ChainIds.POLYGON,
-  ChainIds.ARBITRUM,
-  ChainIds.AVALANCHE,
-  ChainIds.BASE,
-  ChainIds.TELOS,
-  ChainIds.BSC,
-  ChainIds.RSK,
-];
+export const supportedNetworks: number[] = [ChainIds.DEVNET];
